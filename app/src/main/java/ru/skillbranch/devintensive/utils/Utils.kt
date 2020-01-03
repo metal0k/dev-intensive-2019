235b1c8bb496d5fe062a3c81f4952ef2fbb3b056
Denis <denis.k.new@gmail.com>
2020-01-03 19:49:38 +0300

    parseFullName (3)

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -1,5 +1,13 @@
 package ru.skillbranch.devintensive.utils
 
-class Utils {
+import java.util.*
+
+object Utils {
+    fun parseFullName(fullName: String?): Pair<String?, String?> {
+        val parts: List<String?>? = fullName?.split(" ")?.map { if (it.isBlank()) null else it }
+        return parts?.getOrNull(0) to parts?.getOrNull(1)
+    }
 
 }
+
+

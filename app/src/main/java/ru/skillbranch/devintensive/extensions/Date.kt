9b61009473ca6e813eb2d92ca1fe3452626e1628
Denis <denis.k.new@gmail.com>
2020-01-03 20:36:28 +0300

    Date.format (4)

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -1,7 +1,12 @@
 package ru.skillbranch.devintensive.extensions
 
+import java.text.SimpleDateFormat
 import java.util.*
 
 fun Date.humanizeDiff(): String {
     return this.toString()
 }
+
+fun Date.format(pattern: String = "HH:mm:ss dd.MM.yy") =
+    SimpleDateFormat(pattern, Locale("ru")).format(this)
+

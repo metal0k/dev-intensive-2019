a9fa3970596bb73f52e24382456b6452e02ec4f1
Denis <denis.k.new@gmail.com>
2020-01-03 20:37:04 +0300

    Date.add (5)

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -1,5 +1,6 @@
 package ru.skillbranch.devintensive.extensions
 
+import android.text.format.DateUtils
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -10,3 +11,15 @@ fun Date.humanizeDiff(): String {
 fun Date.format(pattern: String = "HH:mm:ss dd.MM.yy") =
     SimpleDateFormat(pattern, Locale("ru")).format(this)
 
+enum class TimeUnits {
+    SECOND, MINUTE, HOUR, DAY
+}
+
+fun Date.add(value: Int, units: TimeUnits): Date = this.apply {
+    time += when (units) {
+        TimeUnits.SECOND -> value * DateUtils.SECOND_IN_MILLIS
+        TimeUnits.MINUTE -> value * DateUtils.MINUTE_IN_MILLIS
+        TimeUnits.HOUR -> value * DateUtils.HOUR_IN_MILLIS
+        TimeUnits.DAY -> value * DateUtils.DAY_IN_MILLIS
+    }
+}

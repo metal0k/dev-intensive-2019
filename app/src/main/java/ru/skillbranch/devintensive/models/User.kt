831040802b113b8b73a9e85c9947237ddf8a95a3
Denis <denis.k.new@gmail.com>
2020-01-03 18:13:32 +0300

    Data class User (1)

--- app/src/main/java/ru/skillbranch/devintensive/models/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -1,5 +1,31 @@
 package ru.skillbranch.devintensive.models
 
-class User {
+import ru.skillbranch.devintensive.utils.Utils
+import java.util.*
 
+data class User(
+    val id: String,
+    var firstName: String?,
+    var lastName: String?,
+    var avatar: String?,
+    var rating: Int = 0,
+    var respect: Int = 0,
+    var lastVisit: Date? = Date(),
+    var isOnline: Boolean = false
+) {
+
+    companion object Factory {
+        var mLastId: Int = -1
+
+        fun makeUser(fullName: String): User {
+            mLastId++
+            val (firstName, lastName) = Utils.parseFullName(fullName)
+            return User(
+                mLastId.toString(),
+                firstName,
+                lastName,
+                null
+            )
+        }
+    }
 }

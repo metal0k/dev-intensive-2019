9ef3110c5cc50cd9dc7ce143d42f13d430c235b8
Denis <denis.k.new@gmail.com>
2020-01-03 22:59:18 +0300

    transliteration (7)

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -19,7 +19,54 @@ object Utils {
                     null
             }
 
+    fun transliteration(payload: String, divider: String = " "): String {
+        val transMap: Map<Char, String> = mapOf(
+            'а' to "a",
+            'б' to "b",
+            'в' to "v",
+            'г' to "g",
+            'д' to "d",
+            'е' to "e",
+            'ё' to "e",
+            'ж' to "zh",
+            'з' to "z",
+            'и' to "i",
+            'й' to "i",
+            'к' to "k",
+            'л' to "l",
+            'м' to "m",
+            'н' to "n",
+            'о' to "o",
+            'п' to "p",
+            'р' to "r",
+            'с' to "s",
+            'т' to "t",
+            'у' to "u",
+            'ф' to "f",
+            'х' to "h",
+            'ц' to "c",
+            'ч' to "ch",
+            'ш' to "sh",
+            'щ' to "sh'",
+            'ъ' to "",
+            'ы' to "i",
+            'ь' to "",
+            'э' to "e",
+            'ю' to "yu",
+            'я' to "ya"
+        )
+        return payload
+            .replace(Regex("\\s+"), divider)
+            .toCharArray().fold("") { acc, chr ->
+                if (transMap.containsKey(chr.toLowerCase()))
+                    acc + (transMap.get(chr.toLowerCase()))?.let { if (chr.isUpperCase()) it.capitalize() else it }
+                else
+                    acc + chr
 
+
+            }
+    }
 }
+
 
 

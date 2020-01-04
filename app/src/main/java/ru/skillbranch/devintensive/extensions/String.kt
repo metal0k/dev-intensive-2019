491a265bc4a4539f4c21957786b67b00cc820c35
Denis <denis.k.new@gmail.com>
2020-01-05 01:08:30 +0300

    truncate (11)

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -1,2 +1,10 @@
 package ru.skillbranch.devintensive.extensions
 
+fun String.truncate(length: Int = 16): String = let {
+    if (it.length > length) {
+        val lastIdx = if (it.get(length-1) == ' ') length-1 else length
+        it.substring(0, lastIdx) + "..."
+    } else it
+
+}
+

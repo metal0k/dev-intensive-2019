b7b79afd33a2abd53120df3a5a537c43b6fe8491
Denis <denis.k.new@gmail.com>
2020-01-05 00:07:55 +0300

    Date.humanizeDiff (8)

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -50,3 +50,32 @@ fun Date.add(value: Int, units: TimeUnits): Date = this.apply {
         TimeUnits.DAY -> value * DateUtils.DAY_IN_MILLIS
     }
 }
+
+const val MINUTE_SECONDS = DateUtils.MINUTE_IN_MILLIS/1000
+const val HOUR_SECONDS = DateUtils.HOUR_IN_MILLIS/1000
+const val DAY_SECONDS = DateUtils.DAY_IN_MILLIS/1000
+
+
+fun Date.humanizeDiff(date: Date = Date()): String = let {
+    val diff = (this.time - date.time) / 1000
+    val absDiff = abs(diff)
+    val result = when (absDiff) {
+        in 0L..1L -> "только что"
+        in 1L..45L -> "несколько секунд"
+        in 45L..75L -> "минуту"
+        in 75L..45L * MINUTE_SECONDS -> TimeUnits.MINUTE.plural((absDiff / MINUTE_SECONDS).toInt())
+        in 45 * MINUTE_SECONDS..75 * MINUTE_SECONDS -> "час"
+        in 75 * MINUTE_SECONDS..22 * HOUR_SECONDS -> TimeUnits.HOUR.plural((absDiff / HOUR_SECONDS).toInt())
+        in 22 * HOUR_SECONDS..26 * HOUR_SECONDS -> "день"
+        in 26 * HOUR_SECONDS..360 * DAY_SECONDS -> TimeUnits.DAY.plural((absDiff / DAY_SECONDS).toInt())
+        else -> if (diff < 0) "более года назад" else "более чем через год"
+    }
+    if (absDiff in 2L..360 * DAY_SECONDS)
+        when {
+            diff > 0 -> "через " + result
+            diff < 0 -> result + " назад"
+            else -> result
+        }
+    else
+        result
+}

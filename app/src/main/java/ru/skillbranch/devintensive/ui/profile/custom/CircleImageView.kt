d25d79dfdfea134c1af2fa56d8ba8ec3a161b898
Denis <denis.k.new@gmail.com>
2020-01-17 21:54:53 +0300

    *CircleImageView

---
+++ app/src/main/java/ru/skillbranch/devintensive/ui/profile/custom/CircleImageView.kt
@@ -0,0 +1,140 @@
+package ru.skillbranch.devintensive.ui.profile.custom
+
+import android.content.Context
+import android.graphics.*
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
+import android.util.AttributeSet
+import android.widget.ImageView
+import androidx.annotation.ColorRes
+import androidx.annotation.Dimension
+import ru.skillbranch.devintensive.R
+import ru.skillbranch.devintensive.extensions.log
+
+class CircleImageView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : ImageView(context, attrs, defStyleAttr) {
+
+    companion object {
+        private const val DEFAULT_BORDER_COLOR = Color.WHITE
+        private const val DEFAULT_BORDER_WIDTH = 2f
+        private val BITMAP_CONFIG = Bitmap.Config.ARGB_8888
+    }
+
+    private var borderColor = DEFAULT_BORDER_COLOR
+    private var borderWidth = DEFAULT_BORDER_WIDTH
+
+
+    init {
+        attrs?.also {
+            val a = context.obtainStyledAttributes(attrs, R.styleable.CircleImageView)
+            borderColor = a.getColor(
+                R.styleable.CircleImageView_cv_borderColor,
+                DEFAULT_BORDER_COLOR
+            )
+            borderWidth = a.getDimension(
+                R.styleable.CircleImageView_cv_borderWidth,
+                DEFAULT_BORDER_WIDTH
+            )
+            log("borderColor: $borderColor, borderWidth: $borderWidth")
+            a.recycle()
+        }
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        drawRoundImage(canvas)
+        drawStroke(canvas)
+    }
+
+    private fun drawStroke(canvas: Canvas) {
+        val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        val radius = width / 2f
+
+        /* Border paint */
+        paint.color = borderColor
+        paint.style = Paint.Style.STROKE
+        paint.strokeWidth = borderWidth
+        canvas.drawCircle(width / 2f, width / 2f, radius - borderWidth / 2f, paint)
+    }
+
+    private fun getBitmapFromDrawable(drawable: Drawable?): Bitmap? {
+        if (drawable == null) {
+            return null
+        }
+
+        if (drawable is BitmapDrawable) {
+            return drawable.bitmap
+        }
+
+        try {
+            val bitmap: Bitmap
+
+//            if (drawable is ColorDrawable) {
+//                bitmap = Bitmap.createBitmap(COLORDRAWABLE_DIMENSION, COLORDRAWABLE_DIMENSION, BITMAP_CONFIG)
+//            } else {
+            bitmap = Bitmap.createBitmap(
+                drawable.intrinsicWidth,
+                drawable.intrinsicHeight,
+                BITMAP_CONFIG
+            )
+//            }
+
+            val canvas = Canvas(bitmap)
+            drawable.setBounds(0, 0, canvas.width, canvas.height)
+            drawable.draw(canvas)
+            return bitmap
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return null
+        }
+
+    }
+
+    private fun drawRoundImage(canvas: Canvas) {
+//        var b: Bitmap = (drawable as BitmapDrawable).bitmap
+//        val bitmap = b.copy(Bitmap.Config.ARGB_8888, true)
+        val bitmap: Bitmap? = getBitmapFromDrawable(drawable)
+        if (bitmap == null)
+            return
+        /* Scale the bitmap */
+        val scaledBitmap: Bitmap
+        val ratio: Float = bitmap.width.toFloat() / bitmap.height.toFloat()
+        val height: Int = Math.round(width / ratio)
+        scaledBitmap = Bitmap.createScaledBitmap(bitmap, width, height, false)
+
+        /* Cutting the outer of the circle */
+        val shader: Shader
+        shader = BitmapShader(scaledBitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP)
+
+        val rect = RectF()
+        rect.set(0f, 0f, width.toFloat(), height.toFloat())
+
+        val imagePaint = Paint()
+        imagePaint.isAntiAlias = true
+        imagePaint.shader = shader
+        canvas.drawRoundRect(rect, width.toFloat(), height.toFloat(), imagePaint)
+    }
+
+    @Dimension
+    fun getBorderWidth(): Int = borderWidth.toInt()
+
+    fun setBorderWidth(@Dimension dp: Int) {
+        borderWidth = dp.toFloat()
+    }
+
+    fun getBorderColor(): Int = borderColor
+
+
+    fun setBorderColor(hex: String) {
+        borderColor = Color.parseColor(hex)
+    }
+
+    fun setBorderColor(@ColorRes colorId: Int) {
+        borderColor = this.context.getColor(colorId)
+
+    }
+
+}

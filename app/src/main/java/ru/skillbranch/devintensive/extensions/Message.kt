808871adfe7e8c4bf965a6863d84518767ab4b88
Denis <denis0kor@gmail.com>
2020-07-24 19:16:37 +0300

    messageShort fix

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Message.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Message.kt
@@ -6,6 +6,6 @@ import ru.skillbranch.devintensive.models.TextMessage
 
 fun BaseMessage?.messageShort(): Pair<String?, String?> = when (this) {
     is TextMessage -> this.text?.trim()?.truncate(64) to this.from.firstName
-    is ImageMessage -> null to this.from.firstName
+    is ImageMessage -> "${this.from.firstName} отправил фото" to this.from.firstName
     else -> null to null
 }
